aa03330f8feee97e4f0af9c14b01de8f5f8d4f76
chengji253 <liuhan253@163.com>
2021-06-11 16:23:39 +0800

    1

--- 1_MATLAB Files/SI TI 2D/Target_velocity.m
+++
@@ -1,7 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This function defines the target velocity
-% Input variable: time t
-% Output variable: target velocity t
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function vt = Target_velocity(t)
-vt = [1;cos(t)];
